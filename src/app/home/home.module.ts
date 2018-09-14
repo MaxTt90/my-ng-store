03c9bdf47b2065c9309afe7ecabe587e5c61a590
Shang, Yitong <yitong.shang@elekta.com>
2018-09-14 16:29:47 +0800

    update

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,24 +1,36 @@
 import { NgModule } from '@angular/core';
 import { NgbModule, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../shared/material/material.module';
+
 import { HeroComponent } from './hero/hero.component';
 import { CategoryComponent } from './category/category.component';
 import { HotFeatureComponent } from './hot-feature/hot-feature.component';
 import { ProductCardComponent } from '../components/product-card/product-card.component';
 import { StartProductsComponent } from './start-products/start-products.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   imports: [
     CommonModule,
     NgbModule,
-    NgbCarouselModule
+    NgbCarouselModule,
+    MaterialModule
   ],
   exports: [
     HeroComponent,
     CategoryComponent,
     HotFeatureComponent,
-    StartProductsComponent
+    StartProductsComponent,
+    MainPageComponent
   ],
-  declarations: [HeroComponent, CategoryComponent, HotFeatureComponent, StartProductsComponent]
+  declarations: [
+    HeroComponent,
+    CategoryComponent,
+    HotFeatureComponent,
+    StartProductsComponent,
+    ProductCardComponent,
+    MainPageComponent
+  ]
 })
 export class HomeModule { }
